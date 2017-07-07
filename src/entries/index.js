f4079fb8d231a7780457651fb9c4ad45b7ed4dac
WilberTian <tianli917@hotmail.com>
2017-07-07 10:46:19 +0800

    add react perf in dev mode

--- src/entries/index.js
+++ src/entries/index.js
@@ -1,7 +1,11 @@
 import indexPage from '../pages/index';
 
-indexPage();
+if (process.env.NODE_ENV !== 'production') {
+    window.Perf = require('react-addons-perf');
+}
 
+indexPage();
+console.log(process.env.NODE_ENV);
 if (module.hot) {
     module.hot.accept('../pages/index', () => {
         const _new = require('../pages/index').default;
